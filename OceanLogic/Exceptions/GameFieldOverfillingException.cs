09bac4168cb8f237af7b72003afe107e3ff25478
darkxp <andrejveselyj90@gmail.com>
2022-07-12 15:45:51 +0300

    Add exception class

---
+++ OceanLogic/Exceptions/GameFieldOverfillingException.cs
@@ -0,0 +1,15 @@
+﻿namespace OceanLogic.Exceptions
+{
+     public class GameFieldOverfillingException : OceanLogicException
+     {
+        public GameFieldOverfillingException() : base("The number of game objects is greater than the playing field!")
+        {
+
+        }
+
+        public GameFieldOverfillingException(string message) : base(message)
+        {
+
+        }
+    }
+}
