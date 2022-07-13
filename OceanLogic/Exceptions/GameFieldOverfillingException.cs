c12ade977acc6ce3ebdd0340a09fc6d2d3e15a5f
darkxp <andrejveselyj90@gmail.com>
2022-07-13 13:30:10 +0300

    Added base construct

--- OceanLogic/Exceptions/GameFieldOverfillingException.cs
+++ OceanLogic/Exceptions/GameFieldOverfillingException.cs
@@ -1,15 +1,19 @@
-﻿namespace OceanLogic.Exceptions
+﻿using System;
+
+namespace OceanLogic.Exceptions
 {
-     public class GameFieldOverfillingException : OceanLogicException
-     {
+    public class GameFieldOverfillingException : Exception
+    {
         public GameFieldOverfillingException() : base("The number of game objects is greater than the playing field!")
         {
-
         }
 
         public GameFieldOverfillingException(string message) : base(message)
         {
+        }
 
+        public GameFieldOverfillingException(string message, Exception inner) : base(message, inner)
+        {
         }
     }
 }
